d3d1c6e34eba41eefb6bf4ad40cb5f002d500dee
Anthony Weyer <antweyer@iu.edu>
2024-02-24 04:02:01 +0000

    v3

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const path = require('path');
 const fs = require ('fs-extra');
 const os = require('os');
 const sharp = require('sharp');
-
+const getExif = require('exif-async');
+const parseDMS = require('parse-dms');
+const { read } = require('fs');
 
 //Entry point fucntion
 exports.generate_thumb_data = async (file, context) =>{
@@ -14,21 +16,100 @@ exports.generate_thumb_data = async (file, context) =>{
     const thumbnailsBucket = storage.bucket('sp24-41200-antweyer-gj-tumbnails');
     const finalBucket = storage.bucket('sp24-41200-antweyer-gj-final');
 
-    //Creates file name
-    const finalFileName = `${gcsFile.generation}`;
-    //creates working directory
-    const workingDir = path.join(os.tmpdir(), 'thmbs');
-    //creates "local" file name
-    const tempFilePath = path.join(workingDir, finalFileName);
-    console.log(`File path: ${tempFilePath}`);
-    //waits for working dir to be ready
-    await fs.ensureDir(workingDir);
-    //downloads OG file to "local" path
-    await sourceBucket.file(gcsFile.name).download({
-        destination: tempFilePath
-    });
-    console.log(`Downloaded file with path of: ${tempFilePath}`);
-    await sourceBucket.file(gcsFile.name).delete({tempFilePath});
+
+
+
+
+    //check if correct file type
+    let fileExtension = '';
+    let vaildFile = false;
+    if (gcsFile.contentType === 'image/jpeg'){
+        console.log('File type is jpg');
+        fileExtension = 'jpg';
+        vaildFile = true
+    }else if (gcsFile.contentType === 'image/png'){
+        console.log('File type is png');
+        fileExtension = 'png';
+        vaildFile = true;
+    }else{
+        console.log('This is not a valid File.')
+        sourceBucket.file(gcsFile.name).delete;
+        return;
+    }
+    if (vaildFile){
+        
+        //Create new file name for final version
+        const finalFileName = `${gcsFile.generation}.${fileExtension}`;
+        console.log(`${finalFileName}`)
+
+        //creates working directory
+        const workingDir = path.join(os.tmpdir(), 'thmbs');
+        console.log(`working directory is created ${workingDir}`)
+
+        //creates "local" file name
+        const tempFilePath = path.join(workingDir, finalFileName);
+        console.log(`Local File path: ${tempFilePath}`);
+
+        //create file name for thumbnail image
+        const thumbName = `thumb@64_${finalFileName}`;
+        console.log(`Thumbnail image name created ${finalFileName}`);
+
+        //create "lcoal" thumbnail path
+        const thumbPath = path.join(workingDir,thumbName);
+        console.log(`local thumbnail path: ${thumbPath}`);
+
+        //waits for working dir to be ready
+        await fs.ensureDir(workingDir);
+
+        // downloads OG file to "local" path
+        await sourceBucket.file(gcsFile.name).download({
+            destination: tempFilePath
+        });
+        console.log(`Downloaded file with path of: ${tempFilePath}`);
+
+
+        
+        //gets GPS data
+        async function readExifData(localFile){
+            try{
+                let exifData =await getExif(localFile);
+                return exifData.gps;
+            }catch(err){
+                console.log(err);
+                return null;
+            }
+        }
+        let gpsObject = await readExifData(tempFilePath);
+        console.log(gpsObject.GPSLatitude);
+        const latStr = parseDMS(`${gpsObject.GPSLatitude[0]}:${gpsObject.GPSLatitude[1]}:${gpsObject.GPSLatitude[2]}`);
+        const lonStr = parseDMS(`${gpsObject.GPSLongitude[0]}:${gpsObject.GPSLongitude[1]}:${gpsObject.GPSLongitude[2]}`);
+        console.log(latStr);
+        console.log(lonStr);
+        
+        
+        
+       //resizes to thumbnail
+        await sharp(tempFilePath).resize(64).withMetadata().toFile(thumbPath).then(async ()=>{
+            await thumbnailsBucket.upload(thumbPath);
+        })
+        console.log(`thumbnail is uploaded to thumbnail bucket ${thumbName}`)
+
+        //upload to final bucket
+        await finalBucket.upload(tempFilePath);
+        console.log(`fullsize image is uploaded to final bucket ${gcsFile.name}`);
+
+        
+
+
+        //deletes working directory on VM
+        await fs.remove(workingDir);
+        console.log(`files deleted from working local dir`);
+        
+        
+    }
+    await console.log(gcsFile.name);
+    // await sourceBucket.file(gcsFile.name).delete();
+    console.log(`Deleted file from uploads bucket: ${gcsFile.name}`);
 
 
 }
